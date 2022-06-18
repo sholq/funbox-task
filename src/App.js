f536a5a5309e86b0beee3062b972059e3d2aaf0e
sholq <semyon.silkin@gmail.com>
2022-06-18 16:05:17 +0300

    docs: init app

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <main className="main">
+        <h1 className="main__header">Ты сегодня покормил кота?</h1>
+      </main>
+    </div>
+  );
+}
+
+export default App;
